6573f709d1d00797d5293c74cbba6c1bdedf9ae7
mhndrachndraa <mhndrachndraa@gmail.com>
2026-05-06 10:31:26 +0700

    menghilangkan kategori

--- Makanan.java
+++ Makanan.java
@@ -1,41 +1,38 @@
 public class Makanan extends Produk {
     private int expired;
+
     public Makanan(String nama, double harga, int stok, String kategori, int expired) {
         super(nama, harga, stok, kategori);
         this.expired = expired;
     }
+
     public int getExpired() {
         return expired;
     }
-    public String getKategori() {
-        return kategori;
-    }
-    
 
     @Override
     public void tampilInfo() {
-        System.out.println("Makanan");
+        System.out.println("Jenis : Makanan");
         System.out.println("Expired : " + expired + " hari");
     }
-    public double hitungDiskon(double harga) {
+
+    public double hitungDiskon(int jumlahBeli) {
         if (jumlahBeli >= 10) {
-            return hitungTotalHarga(jumlahBeli) * 0.05; // Diskon 10% untuk pembelian 10 atau lebih
+            return hitungTotalHarga(jumlahBeli) * 0.1; // 10%
         }
         return 0;
     }
+
     public double hitungTotalHargaDiskon(int jumlahBeli, double persenDiskon) {
         return hitungTotalHarga(jumlahBeli) * (persenDiskon / 100);
     }
+
     public double hitungHargaSetelahDiskon(int jumlahBeli, double persenDiskon) {
         return hitungTotalHarga(jumlahBeli) - hitungTotalHargaDiskon(jumlahBeli, persenDiskon);
     }
 
-
-
     @Override
     public double hitungPajak(double harga) {
-        return harga * 0.1; // Pajak 10% untuk makanan
+        return harga * 0.1;
     }
-
-    
 }
