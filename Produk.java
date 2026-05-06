d7402becc20515ddf05f2b16c299dee1869d4504
mhndrachndraa <mhndrachndraa@gmail.com>
2026-05-06 10:12:28 +0700

    penambahan if else

--- Produk.java
+++ Produk.java
@@ -28,7 +28,20 @@ abstract class Produk {
     public double hitungTotalHarga(int jumlahBeli){
         return harga * jumlahBeli;
     }
-    
+    public void beli(int jumlahBeli){
+        if (jumlahBeli <= 0) {
+            System.out.println("Jumlah pembelian tidak valid harus lebih dari 0.");
+            return;
+        } 
+        if (jumlahBeli > stok) {
+            System.out.println("Stok" + nama + " tidak cukup. Stok tersedia: " + stok);
+            return;
+        } else {
+            stok -= jumlahBeli;
+            System.out.println("Pembelian berhasil. Total harga: Rp." + hitungTotalHarga(jumlahBeli) + ". Sisa stok: " + stok);
+        }
+        
+    }
 
 
     public void tampilInfo(){
