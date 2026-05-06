98715e655395e1065d8842764bb53210f3726ede
mhndrachndraa <mhndrachndraa@gmail.com>
2026-05-06 10:11:25 +0700

    penambahan metode

--- Produk.java
+++ Produk.java
@@ -25,7 +25,10 @@ abstract class Produk {
     public void setStok(int stok){
         this.stok = stok;
     }
-   
+    public double hitungTotalHarga(int jumlahBeli){
+        return harga * jumlahBeli;
+    }
+    
 
 
     public void tampilInfo(){
