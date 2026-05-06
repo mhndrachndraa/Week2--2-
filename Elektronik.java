aab648d5f2066ddde2d9313ddd310cf63a6b7587
mhndrachndraa <mhndrachndraa@gmail.com>
2026-05-06 10:36:55 +0700

    penamabahan atribut pada class elektronik

--- Elektronik.java
+++ Elektronik.java
@@ -3,4 +3,12 @@ class Elektronik extends Produk {
     private int garansiTahun;
     private static final double PPN = 0.11;
 
+    public Elektronik(String nama, String merek, double harga,
+        int stok, int garansiTahun) {
+        super(nama, harga, stok, "Elektronik");
+        this.merek = merek;
+        this.garansiTahun = garansiTahun;
+    }
+
+   
 }
